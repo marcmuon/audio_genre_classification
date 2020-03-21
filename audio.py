c55225503c6f1e26d5b25c823fc765fca6128d92
Marc Kelechava <marc.kelechava@gmail.com>
2020-03-21 13:00:54 -0700

    minor edits

--- audio.py
+++ audio.py
@@ -1,5 +1,6 @@
 import librosa
 import numpy as np
+import pickle
 
 
 class Audio:
@@ -17,7 +18,7 @@ class Audio:
             [self.features, feature]
             if self.features is not None else feature)
 
-    def extract_mfcc(self, n_mfcc=12):
+    def _extract_mfcc(self, n_mfcc=12):
         """
         Extract MFCC mean and std_dev vecs for a clip.
         Appends (2*n_mfcc,) shaped vector to self.features
@@ -31,7 +32,7 @@ class Audio:
         mfcc_feature = np.hstack([mfcc_mean, mfcc_std])
         self._concat_features(mfcc_feature)
 
-    def extract_spectral_contrast(self, n_bands=3):
+    def _extract_spectral_contrast(self, n_bands=3):
         """
         Extract Spectral Contrast mean and std_dev vecs for a clip.
         Appends (2*(n_bands+1),) shaped vector to self.features
@@ -45,13 +46,36 @@ class Audio:
         spec_con_feature = np.hstack([spec_con_mean, spec_con_std])
         self._concat_features(spec_con_feature)
 
-    def extract_tempo(self):
+    def _extract_tempo(self):
         """
         Extract the BPM.
         Appends (1,) shaped vector to instance feature vector
         """
         tempo = librosa.beat.tempo(y=self.y, sr=self.sr)
         self._concat_features(tempo)
+
+    def extract_features(self):
+        """
+        Populates self.features with extracted audio features
+        """
+
+        self._extract_mfcc()
+        self._extract_spectral_contrast()
+        self._extract_tempo()
+
+        def __cleaner():
+            self._delete_raw_sample()  # self.y too large to keep
+            self._save_to_disk()
+
+        __cleaner()
+
+    def _delete_raw_sample(self):
+        del self.y
+        del self.sr
+
+    def _save_to_disk(self):
+        with open(f"data/{self.path}.pkl", "wb") as output_file:
+            pickle.dump(self.features, output_file)
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -5,34 +5,39 @@ import pandas as pd
 
 def get_audio_metadata(playlist):
     """
-    Assumes an Apple Music playlist saved as plain text.
+    Currently assumes an Apple Music playlist saved as plain text.
+    Returns: zip object with (audio_paths, labels)
     """
     df = pd.read_csv(playlist, sep='\t')
     df = df[['Location', 'My Rating']]
-    df['My Rating'] = df['My Rating'].map({
+
+    rating_map = {
         100.0: 5,
         80.0: 4,
         60.0: 3,
         40.0: 2,
-        20.0: 1})
+        20.0: 1
+    }
+    df['My Rating'] = df['My Rating'].map(rating_map)
 
     if df['Location'][0].startswith('Macintosh HD'):
         df['Location'] = df['Location'].str.replace('Macintosh HD', '')
 
     paths = df['Location'].values
     labels = df['My Rating'].values
-    metadata = zip(paths, labels)
 
-    return metadata
+    return zip(paths, labels)
 
 
 if __name__ == "__main__":
 
     all_metadata = get_audio_metadata(playlist='data/labeled_TEST.txt')
-    audio_librosa = [Audio(metadata) for metadata in all_metadata]
-    for audio in audio_librosa:
-        # Calling these methods populate 'audio.features'
-        audio.extract_mfcc()
-        audio.extract_spectral_contrast()
-        audio.extract_tempo()
-    feature_matrix = np.vstack([audio.features for audio in audio_librosa])
+    audio_files = [Audio(metadata) for metadata in all_metadata]
+    for audio in audio_files:
+        audio.extract_features()  # populates audio.features attribute
+    feature_matrix = np.vstack([audio.features for audio in audio_files])
+    labels = [audio.label for audio in audio_files]
+    dev_path = [audio.path for audio in audio_files]  # delete later
+
+    # TODO - method to skip processing if path already exists in Audio
+    # TODO - add save method for audio and delete it after extraction
