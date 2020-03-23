d91944f81f8dd1e2d9e2a74b95500c4c3dfe85e5
Marc Kelechava <marc.kelechava@gmail.com>
2020-03-22 18:50:15 -0700

    working model, some issues with metric aggregation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from audio import AudioFeature
+from model import Model
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 
 
 def get_audio_metadata(playlist):
@@ -8,6 +11,9 @@ def get_audio_metadata(playlist):
     Currently assumes an Apple Music playlist saved as plain text.
     Returns: zip object with (audio_paths, genre_labels)
     """
+
+    # TODO - assert check that length > intended offset+duration
+
     df = pd.read_csv(playlist, sep='\t')
     df = df[['Location', 'Genre']]
 
@@ -29,5 +35,35 @@ if __name__ == "__main__":
         audio.extract_features(save=True)
         audio_features.append(audio)
 
+    # TODO - implement cache loader for data previously saved to disk
+
     feature_matrix = np.vstack([audio.features for audio in audio_features])
     genre_labels = [audio.genre_label for audio in audio_features]
+
+    le = LabelEncoder()
+    labels = le.fit_transform(genre_labels)
+
+    model_cfg = dict(
+        tt_dict=dict(shuffle=True, test_size=.2),
+        scaler=StandardScaler(copy=True),
+        base_model=RandomForestClassifier(random_state=42,
+                                          n_jobs=4,
+                                          class_weight='balanced',
+                                          n_estimators=250,
+                                          bootstrap=True),
+        param_grid=dict(model__criterion=['entropy', 'gini'],
+                        model__max_features=['log2', 'sqrt'],
+                        model__min_samples_leaf=np.arange(2, 5)),
+        grid_dict=dict(n_jobs=4,
+                       refit=True,
+                       iid=False),
+        kf_dict=dict(n_splits=3,
+                     random_state=42,
+                     shuffle=True)
+    )
+
+    model = Model(feature_matrix, labels, model_cfg)
+    model.run_cv_trials(n_trials=2)
+    model.predict_from_holdout()
+
+    # TODO need more jazz and ambient examples!
---
+++ model.py
@@ -0,0 +1,121 @@
+from sklearn.pipeline import Pipeline
+
+from sklearn.model_selection import (
+    train_test_split,
+    GridSearchCV,
+    StratifiedKFold,
+)
+from sklearn.metrics import confusion_matrix
+import numpy as np
+
+
+class Model:
+
+    def __init__(self, feature_matrix, labels, cfg):
+
+        self.X = feature_matrix
+        self.y = labels
+        self.cfg = cfg
+
+        # populated in .run_cv_trials()
+        self.best_estimators = None
+        self.holdout_test_sets = None
+
+        # populated in .predict()
+        self.fnr = None
+        self.fpr = None
+        self.accuracy = None
+        self.y_preds = None
+
+    def run_cv_trials(self, n_trials=1):
+        """
+        Using Pipeline objects as they don't leak transformations
+        into the validation folds as shown here: https://bit.ly/2N7rdQ0,
+        and here: https://bit.ly/346THQL
+
+        The trials iteration creates a unique train_test_split, hence
+        a unique heldout test. We save this test set for predictions
+        later on, and then we'll average the heldout test predictions
+        (via their associated best_estimator) for a better sense of
+        generalization error.
+
+        Note that return_train_score=True and verbose=3 in GridSearchCV
+        is useful for debugging as you'll see if your models are overfitting
+        badly.
+        """
+        self.best_estimators = []
+        self.holdout_test_sets = []
+
+        for i in range(n_trials):
+
+            X_cv, X_test, y_cv, y_test = train_test_split(
+                self.X,
+                self.y,
+                random_state=i,
+                stratify=self.y,
+                **self.cfg['tt_dict'])
+
+            pipe = Pipeline([
+                ('scaler', self.cfg['scaler']),
+                ('model', self.cfg['base_model'])
+            ])
+
+            kf = StratifiedKFold(**self.cfg['kf_dict'])
+
+            grid_search = GridSearchCV(estimator=pipe,
+                                       param_grid=self.cfg['param_grid'],
+                                       cv=kf,
+                                       scoring='balanced_accuracy',
+                                       return_train_score=True,
+                                       verbose=3,
+                                       **self.cfg['grid_dict'])
+
+            grid_search.fit(X_cv, y_cv)
+            best_estimator = grid_search.best_estimator_
+
+            self.best_estimators.append(best_estimator)
+            self.holdout_test_sets.append((X_test, y_test))
+
+    def predict_from_holdout(self):
+
+        self.fpr = []
+        self.fnr = []
+        self.accuracy = []
+        self.y_preds = []
+
+        for i, test in enumerate(self.holdout_test_sets):
+
+            X_test, y_test = test[0], test[1]
+            scaler = self.best_estimators[i]['scaler']
+            model = self.best_estimators[i]['model']
+
+            X_test_scaled = scaler.transform(X_test)
+
+            y_pred = model.predict(X_test_scaled)
+
+            cnf_matrix = confusion_matrix(y_test, y_pred)
+
+            FP = cnf_matrix.sum(axis=0) - np.diag(cnf_matrix)
+            FN = cnf_matrix.sum(axis=1) - np.diag(cnf_matrix)
+            TP = np.diag(cnf_matrix)
+            TN = cnf_matrix.sum() - (FP + FN + TP)
+
+            FP = FP.astype(float)
+            FN = FN.astype(float)
+            TP = TP.astype(float)
+            TN = TN.astype(float)
+
+            print(f'Test Set from Trial Number {i}:')
+            print(f'TN:{TN}, FP:{FP}, FN:{FN}, TP:{TP}')
+
+            self.fpr.append(FP / (FP + TN))
+            self.fnr.append(FN / (TP + FN))
+            self.accuracy.append((TP + TN) / (TP + TN + FP + FN))
+
+        print(f'Averages over all Test Sets:')
+        print(f'False Positive Rate: {self.fpr}')
+        print(f'False Negative Rate: {self.fnr}')
+        print(f'Accuracy: {self.accuracy}')
+
+    # def save(self):
+        # pass
